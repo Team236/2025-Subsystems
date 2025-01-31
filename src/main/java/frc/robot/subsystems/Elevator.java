39554325a917c9af6f6c1fe21bbd0c6d4e39a3f0
236-1 <sburnham3@gmail.com>
2025-01-30 19:10:06 -0500

    finished updating for TalonFX

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -6,6 +6,7 @@ package frc.robot.subsystems;
 
 import frc.robot.Constants;
 
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
@@ -19,17 +20,151 @@ import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+// // SPARK MAX VERSION (COMMENT WHEN NOT USING)
+// public class Elevator extends SubsystemBase {
+//   /** Creates a new Elevator. */
+
+//   // SPARK MAX
+//   private SparkMax leftElevatorMotor, rightElevatorMotor;
+//   private SparkMaxConfig leftConfig, rightConfig;
+//   public RelativeEncoder leftElevEncoder, rightElevEncoder;
+
+//   public Elevator() {
+//     // SPARK MAX
+
+//     leftElevatorMotor = new SparkMax(Constants.MotorControllers.ID_ELEVATOR_LEFT, MotorType.kBrushless);
+//     rightElevatorMotor = new SparkMax(Constants.MotorControllers.ID_ELEVATOR_RIGHT, MotorType.kBrushless);
+
+//     // configure motors
+//     leftConfig = new SparkMaxConfig();
+//     leftConfig.inverted(true);
+//     leftConfig.smartCurrentLimit(Constants.MotorControllers.SMART_CURRENT_LIMIT);
+
+//     rightConfig = new SparkMaxConfig();
+//     rightConfig.inverted(false);
+//     rightConfig.smartCurrentLimit(Constants.MotorControllers.SMART_CURRENT_LIMIT);
+
+//     leftElevatorMotor.configure(leftConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+//     rightElevatorMotor.configure(rightConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+
+//     leftElevEncoder = leftElevatorMotor.getEncoder();
+//     rightElevEncoder = rightElevatorMotor.getEncoder();
+
+//     try {
+//       elevatorTopLimit = new DigitalInput(Constants.Elevator.DIO_ELEV_TOP);
+//     } catch (Exception e) {
+//       isTException = true;
+//       SmartDashboard.putBoolean("exception thrown for elev bottom limit: ", isTException);
+//     }
+
+//     try {
+//       elevatorBottomLimit = new DigitalInput(Constants.Elevator.DIO_ELEV_BOTTOM);
+//     } catch (Exception e) {
+//       isBException = true;
+//       SmartDashboard.putBoolean("exception thrown for elev bottom limit: ", isBException);
+//     }
+
+//   }
+
+//   public void stopElevator() {
+//     leftElevatorMotor.set(0);
+//     rightElevatorMotor.set(0);
+//   }
+//   public boolean isETopLimit() {
+//     if (isTException) {
+//       return true;
+//     } else {
+//       return elevatorTopLimit.get();
+//     }
+//   }
+//   public boolean isEBotLimit() {
+//     if (isBException) {
+//       return true;
+//     } else {
+//       return elevatorBottomLimit.get();
+//     }
+//   }
+
+//   // reset/zero encoders
+//   public void resetElevatorEncoders(){
+//     // // SPARK MAX
+//     // leftElevEncoder.setPosition(0);
+//     // rightElevEncoder.setPosition(0);
+//   }
+
+//   //returns encoder position in REVOLUTIONS (number of rotations)
+//   public double getElevLeftEncoder() {
+//     return leftElevEncoder.getPosition(); //for a SparkMax encoder
+//   }
+//     public double getElevRightEncoder() {
+//     return rightElevEncoder.getPosition(); //for a SparkMax encoder
+//   }
+
+//   // currently returns revolutions of encoders -- NOT CONVERTED TO IN.
+//   public double getElevatorHeight(){
+//     return Constants.Elevator.ELEV_REV_TO_IN * ((getElevLeftEncoder() + getElevRightEncoder()) / 2);
+//   }
+
+//   // check if elevator is at "top" according to user definition
+//   public boolean isTop() {
+//     boolean eTop;
+//     if (getElevatorHeight() >= Constants.Elevator.MAX_HEIGHT) {
+//       eTop = true;
+//     } else {
+//       eTop = false;
+//     }
+//     return eTop;
+//   }
+
+//   public void setElevSpeed(double speed) {
+//     if (speed > 0) {  
+//       if (isETopLimit() || isTop()) {
+//           // if elevator limit is tripped or elevator is near the top limit switch going up, stop 
+//           stopElevator();
+//        }  else {
+//           // elevator going up but top limit is not tripped, go at commanded speed
+//           leftElevatorMotor.set(speed);
+//           rightElevatorMotor.set(speed);
+//         }
+//       } 
+//     else {
+//       if (isEBotLimit()) {
+//         //elevator going down and is at the bottom,stop and zero encoder
+//         stopElevator();
+//         resetElevatorEncoders();
+//       } else {
+//       // elevator going down but not at the bottom, go at commanded speed
+//         leftElevatorMotor.set(speed);
+//         rightElevatorMotor.set(speed);
+//       }
+//     }
+//   } 
+
+//   public double getElevatorLeftSpeed() {
+//     return leftElevatorMotor.get();
+//   }  
+
+//   public double getElevatorRightSpeed() {
+//     return rightElevatorMotor.get();
+//   }  
+
+//   @Override
+//   public void periodic() {
+//     // This method will be called once per scheduler run
+//     SmartDashboard.putNumber("Elevator height: ", getElevatorHeight());
+//     SmartDashboard.putBoolean("Elevator at top? ", isETopLimit());
+//     SmartDashboard.putBoolean("Elevator at bottom? ", isEBotLimit());
+//   }
+// }
+
+// TALONFX (COMMENT WHEN NOT USING)
 public class Elevator extends SubsystemBase {
   /** Creates a new Elevator. */
-
-  // // SPARK MAX
-  // private SparkMax leftElevatorMotor, rightElevatorMotor;
-  // private SparkMaxConfig leftConfig, rightConfig;
-  // public RelativeEncoder leftElevEncoder, rightElevEncoder;
 
   // TALON FX
   private TalonFX leftElevatorMotor, rightElevatorMotor;
@@ -39,26 +174,6 @@ public class Elevator extends SubsystemBase {
   private boolean isTException, isBException;
 
   public Elevator() {
-    // // SPARK MAX
-
-    // leftElevatorMotor = new SparkMax(Constants.MotorControllers.ID_ELEVATOR_LEFT, MotorType.kBrushless);
-    // rightElevatorMotor = new SparkMax(Constants.MotorControllers.ID_ELEVATOR_RIGHT, MotorType.kBrushless);
-
-    // // configure motors
-    // leftConfig = new SparkMaxConfig();
-    // leftConfig.inverted(true);
-    // leftConfig.smartCurrentLimit(Constants.MotorControllers.SMART_CURRENT_LIMIT);
-
-    // rightConfig = new SparkMaxConfig();
-    // rightConfig.inverted(false);
-    // rightConfig.smartCurrentLimit(Constants.MotorControllers.SMART_CURRENT_LIMIT);
-
-    // leftElevatorMotor.configure(leftConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
-    // rightElevatorMotor.configure(rightConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
-
-    // leftElevEncoder = leftElevatorMotor.getEncoder();
-    // rightElevEncoder = rightElevatorMotor.getEncoder();
-    
     // TALON FX
     leftElevatorMotor = new TalonFX(Constants.MotorControllers.ID_ELEVATOR_LEFT_TALON);
     rightElevatorMotor = new TalonFX(Constants.MotorControllers.ID_ELEVATOR_RIGHT_TALON);
@@ -121,20 +236,19 @@ public class Elevator extends SubsystemBase {
 
   // reset/zero encoders
   public void resetElevatorEncoders(){
-    // // SPARK MAX
-    // leftElevEncoder.setPosition(0);
-    // rightElevEncoder.setPosition(0);
-
     // TALON FX
-
+    leftElevatorMotor.setPosition(0);
+    rightElevatorMotor.setPosition(0);
   }
 
   //returns encoder position in REVOLUTIONS (number of rotations)
   public double getElevLeftEncoder() {
-    return leftElevEncoder.getPosition(); //for a SparkMax encoder
+    StatusSignal<Angle> revs = leftElevatorMotor.getPosition();
+    return revs.getValueAsDouble();
   }
-    public double getElevRightEncoder() {
-    return rightElevEncoder.getPosition(); //for a SparkMax encoder
+  public double getElevRightEncoder() {
+    StatusSignal<Angle> revs = rightElevatorMotor.getPosition();
+    return revs.getValueAsDouble();
   }
 
   // currently returns revolutions of encoders -- NOT CONVERTED TO IN.
